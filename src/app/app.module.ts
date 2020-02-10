787acc109bfdc3711f51fbb5d3c99c3758be2ba5
Luka Orelj <luka.o@hotmail.com>
2020-02-10 21:31:26 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
+import { EventsAppComponent } from './events-app.component';
+import {EventListComponent} from './event/event-list.component'
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/event/event-list.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core'
+
+
+@Component({
+  selector: "events-list",
+  templateUrl: './event-list.component.html'
+}
+)
+export class EventListComponent
+{
+  event = {
+    id:1,
+    name:'First event',
+    date:'9/26/2036',
+    time:'10:00 am',
+    price:599.99,
+    imageUrl:'/assets/images/angularconnect-shield.png',
+    location:{
+      address:'1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  };
+
+}
